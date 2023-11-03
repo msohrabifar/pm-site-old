a5252aa18c009e34d6ba3deda075fcf01b8546f5
peymanprd <dev.pirzadeh@gmail.com>
2023-11-03 13:44:27 +0330

    fix: favicon + logo

--- src/app/therapist/page.tsx
+++ src/app/therapist/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import DownloadIcon from '@/components/icons/Download'
 import AndroidIcon from '@/components/icons/Android'
 
 export default function Therapist() {
   return (
     <div className="flex flex-col justify-center items-center gap-6 mt-16">
-      <Image src="/logo.png" alt="Payeshman Logo" width="64" height="64" />
-      <div className="flex flex-col items-center gap-4 mt-4">
+      <Image src="/logo.png" alt="Payeshman Logo" width="96" height="96" />
+      <div className="flex flex-col items-center gap-4">
         <h1 className="text-4xl font-bold">پایــش مـن</h1>
         <h2 className="text-xl font-medium py-1.5 px-3 text-blue-600 bg-blue-50 rounded-lg">
           نســــخه درمــانگـر
