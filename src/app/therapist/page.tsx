a8ac11b5857683e9d2cb7dac5c323f6210a4533d
peymanprd <dev.pirzadeh@gmail.com>
2023-11-03 11:35:51 +0330

    fix: therapist page change download icon to android

--- src/app/therapist/page.tsx
+++ src/app/therapist/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import DownloadIcon from '@/components/icons/Download'
+import AndroidIcon from '@/components/icons/Android'
 
 export default function Therapist() {
   return (
@@ -19,16 +20,14 @@ export default function Therapist() {
       <Link
         href="https://cdn.payeshman.com/lib/app/payeshman-therapist-1.0.0.apk"
         target="_blank"
-        className="flex items-center gap-4 py-3.5 px-4 rounded-xl bg-blue-600 hover:bg-blue-700 transition-all mt-4"
+        className="flex items-center gap-3 py-3 px-4 rounded-xl bg-blue-600 hover:bg-blue-700 transition-all mt-4"
       >
         <div>
-          <DownloadIcon width={48} height={48} className="text-blue-100" />
+          <AndroidIcon width={40} height={40} className="text-blue-50" />
         </div>
-        <div className="flex flex-col justify-center items-start gap-1">
-          <p className="text-xl text-white font-normal">دانلود نسخه درمانگر</p>
-          <div className="text-xs text-white/60">
-            قابل اجرا روی سیستم عامل اندروید
-          </div>
+        <div className="flex flex-col justify-center items-start gap-0.5">
+          <p className="text-xl text-white font-normal">دانلود مستقیم</p>
+          <div className="text-xs text-white/60">فایل نصب APK</div>
         </div>
       </Link>
     </div>
