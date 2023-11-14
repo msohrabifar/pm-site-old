8555f284e1e43dfd762913d26e376b63833f90ed
peymanprd <dev.pirzadeh@gmail.com>
2023-11-14 23:52:39 +0330

    fix cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,73 @@ import Faqs from '@/components/Faqs'
 import LatestPosts from '@/components/LatestPosts'
 import MagFooter from '@/components/MagFooter'
 
+const ctaSections = [
+  {
+    image: {
+      src: '/shot5.png',
+      alt: 'services'
+    },
+    label: 'خدمات آموزشی',
+    caption: 'پوشش کامل نیازهای آموزشی بیماران',
+    description:
+      'آموزش های تخصصی در زمینه مراقبت، تغذیه، ورزش، فعالیت و مهارت ها'
+  },
+  {
+    image: {
+      src: '/shot5.png',
+      alt: ''
+    },
+    label: '',
+    caption: 'یادگیری ساده و آسان',
+    description:
+      'ارائه آموزش ها با لحنی روان، ساده و استفاده حداقلی از واژگان تخصصی'
+  },
+  {
+    image: {
+      src: '/shot5.png',
+      alt: ''
+    },
+    label: '',
+    caption: 'هرآنچه درمورد داروهایاتان باید بدانید',
+    description: 'آموزش ساده و کاربردی در مورد داروها و نکات مصرف آنها'
+  },
+  {
+    image: {
+      src: '/shot5.png',
+      alt: ''
+    },
+    label: '',
+    caption: 'گفتگوی آسان با کارشناسان حوزه سلامت شات',
+    description:
+      'هر زمان از بخش گفتگو می توانید با کارشناسان متخصص حوزه سلامت در ارتباط باشید و سوالات و مشکلات خود را مطرح کنید.'
+  },
+  {
+    image: {
+      src: '/shot5.png',
+      alt: ''
+    },
+    label: '',
+    caption: 'دسترسی سریع به آموزش های مهم',
+    description: 'با قابلیت نشان ، به آموزش های مهم سریع دسترسی پیدا کنید'
+  }
+]
+
 export default function Home() {
   return (
     <>
       <MainNavbar />
       <MainHero />
       <States />
-      <CTASection />
+      {ctaSections.map((ctaSection, ctaSectionIndex) => (
+        <CTASection
+          key={ctaSectionIndex}
+          image={ctaSection.image}
+          imageAlign={ctaSectionIndex % 2 === 0 ? 'right' : 'left'}
+          label={ctaSection.label}
+          caption={ctaSection.caption}
+          description={ctaSection.description}
+        />
+      ))}
       <Faqs />
       <LatestPosts />
       <MagFooter />
--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -1,38 +1,74 @@
-export default function CTASection() {
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+
+interface CtaImage {
+  src: string
+  alt: string
+}
+
+interface Props {
+  image: CtaImage
+  imageAlign: 'left' | 'right'
+  label: string
+  caption: string
+  description: string
+}
+
+export default function CTASection({
+  image,
+  imageAlign,
+  label,
+  caption,
+  description
+}: Props) {
   return (
     <section className="py-14">
       <div className="max-w-screen-xl mx-auto md:px-8">
-        <div className="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
+        <div
+          className={cn(
+            'items-center gap-x-8 sm:px-4 md:px-0 lg:flex',
+            imageAlign === 'left' ? 'lg:flex-row-reverse' : 'lg:flex-row'
+          )}
+        >
           <div className="sm:hidden lg:block">
-            <div className="w-[460px] h-[460px] rounded-2xl bg-gray-200"></div>
+            <Image src={image.src} width={400} height={400} alt={image.alt} />
           </div>
           <div className="flex-1 max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
-            <h3 className="text-indigo-600 font-semibold">
-              <span className="py-1 px-2 inline-flex items-center gap-x-1 text-sm font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
-                <svg
-                  className="flex-shrink-0 w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="28"
-                  height="28"
-                  viewBox="0 0 24 24"
+            <div className="text-blue-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="80"
+                height="80"
+                viewBox="0 0 24 24"
+              >
+                <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
                   stroke-linecap="round"
-                  stroke-linejoin="round"
+                  stroke-width="1.5"
                 >
-                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
-                  <path d="m9 12 2 2 4-4" />
-                </svg>
-                خدمات آموزشی
+                  <path d="M3.092 6.637L9.782 3.5a5.17 5.17 0 0 1 4.435 0l6.691 3.137c1.456.682 1.456 3.044 0 3.726l-6.69 3.137a5.17 5.17 0 0 1-4.435 0L5 11.258" />
+                  <path
+                    stroke-linejoin="round"
+                    d="M2.5 15v-2.862c0-1.28 0-1.92.33-2.42c.33-.501.919-.753 2.094-1.257L6 8"
+                    opacity=".5"
+                  />
+                  <path
+                    d="M19 11.5v5.125c0 1.008-.503 1.952-1.385 2.44C16.146 19.88 13.796 21 12 21c-1.796 0-4.146-1.121-5.615-1.934C5.504 18.577 5 17.633 5 16.626V11.5"
+                    opacity=".5"
+                  />
+                </g>
+              </svg>
+            </div>
+            {/* <h3 className="text-indigo-600 font-semibold">
+              <span className="py-1 px-2 inline-flex items-center gap-x-1 text-sm font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
+                {label}
               </span>
-            </h3>
-            <p className="text-gray-800 text-xl font-bold sm:text-3xl">
-              پوشش کامل نیازهای آموزشی بیماران
+            </h3> */}
+            <p className="text-gray-800 text-xl font-bold sm:text-3xl !mt-8">
+              {caption}
             </p>
-            <p className="mt-3 text-gray-600 leading-7">
-              آموزش های تخصصی در زمینه مراقبت، تغذیه، ورزش، فعالیت و مهارت ها
-            </p>
+            <p className="mt-3 text-gray-600 leading-7">{description}</p>
           </div>
         </div>
       </div>
